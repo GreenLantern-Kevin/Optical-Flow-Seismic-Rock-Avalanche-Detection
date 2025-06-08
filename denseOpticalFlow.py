7ffc3f768d8139f2674d13d9c462dd01d4544fb3
GreenLantern-Kevin <61045748+GreenLantern-Kevin@users.noreply.github.com>
2025-06-08 23:33:16 +0800

    Update denseOpticalFlow.py

--- denseOpticalFlow.py
+++ denseOpticalFlow.py
@@ -38,6 +38,22 @@ def draw_hsv(flow):
 
     return bgr
 
+def draw_h_channel(flow):      
+    h, w = flow.shape[:2]
+    fx, fy = flow[:, :, 0], flow[:, :, 1]
+
+    ang = np.arctan2(fy, fx) + np.pi
+    h_channel = (ang * (180 / np.pi / 2)).astype(np.uint8)
+
+    return cv2.cvtColor(h_channel, cv2.COLOR_GRAY2BGR), h_channel
+
+def draw_v_channel(flow):       
+    fx, fy = flow[:, :, 0], flow[:, :, 1]
+    v_channel = np.sqrt(fx * fx + fy * fy)
+
+    v_channel = np.minimum(v_channel * 4, 255).astype(np.uint8)
+    return cv2.cvtColor(v_channel, cv2.COLOR_GRAY2BGR), v_channel
+
 
 cap = cv2.VideoCapture("Nayong.mp4")
 fourcc = cv2.VideoWriter_fourcc(*'XVID')        
@@ -73,14 +89,15 @@ while True:
 
     print(f"{fps:.2f} FPS")
 
+    draw_h, h_channel = draw_h_channel(flow)
+    draw_v, v_channel = draw_v_channel(flow)
    
-    stackedImg = cvzone.stackImages([draw_h_channel(flow), draw_v_channel(flow)], cols=2, scale=1)  
+    stackedImg = cvzone.stackImages([draw_flow(gray, flow), draw_hsv(flow), draw_h, draw_v], cols=2, scale=1)  
     out.write(stackedImg)  
     cv2.imshow('flow', draw_flow(gray, flow))
     cv2.imshow('flow HSV', draw_hsv(flow))
 
     frame_number += 1  
-
     
     key = cv2.waitKey(1)
     if key == ord('q'):
